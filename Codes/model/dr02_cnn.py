bf3408845060199f8c84e8951d95b1e0c7c5974e
mdfahimhasan <77580408+mdfahimhasan@users.noreply.github.com>
2025-02-10 09:52:56 -0700

    rearranged

--- Codes/model/dr02_cnn.py
+++ Codes/model/dr02_cnn.py
@@ -8,11 +8,42 @@ from os.path import dirname, abspath
 
 sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 
-from Codes.model.cnn_utils import main, plot_learning_curve, test, unstandardize_save_and_test, plot_shap_values
+from Codes.model.utils_cnn import main, plot_learning_curve, test, unstandardize_save_and_test, plot_shap_values
 
 if __name__ == '__main__':
     # model version
-    model_version = 'v5'                                                 #####
+    model_version = 'v6'                                                #####
+
+    # model switches
+    tune_params = False                                 ################################################################
+    n_trials_for_tuning = 200                           ################################################################
+    implement_earlyStopping = True                     #################################################################
+    plot_hyperparam_importance = True                  #################################################################
+    skip_unstandardizing_training = True               #################################################################
+    skip_unstandardizing_testing = False                #################################################################
+    skip_plot_SHAP_plot = False                         #################################################################
+
+    # default variables (from hyperparameter tuning process)
+    batch_size = 128                                                    ##### batch size of DataLoader
+    n_features = 15                                                     ##### number of input channel in a tile
+    n_epochs = 90                                                       #####
+    input_size = 7                                                      ##### height/width dim of a tile
+    padding = 'same'                                                    #####
+    activation = 'relu'                                                 #####
+    pooling = 'avgpool'                                                 #####
+    patience = 10                                                       ##### early stopping counter patient set to 10 epoch
+    start_earlyStopping_at_epoch = 40                                   ##### early stopping will initialize after 40 epochs
+
+
+    # default model architecture
+    default_params = {
+        'filters': [64, 64],                                            ##### convolutional layers
+        'kernel_size': [5, 5],                                          ##### kernel size for each Conv layer
+        'fc_units': [256, 32],                                          ##### fully connected layer
+        'lr': 0.0003011646825680101,                                    ##### learning rate
+        'weight_decay': 6.86933341560855e-05,                           ##### weight decay
+        'dropout': 0.5                                                  ##### dropout rate
+    }
 
 
     # directories
@@ -32,39 +63,9 @@ if __name__ == '__main__':
     hyperparam_importance_plot = f'../../Model_run/DL_model/hyperparam_imp_{model_version}.jpg'
     leaning_curve_plot = f'../../Model_run/DL_model/learning_curve_{model_version}.jpg'
 
-
-    # Default variables (from hyperparameter tuning process)
-    batch_size = 128                                                    ##### batch size of DataLoader
-    n_features = 15                                                     ##### number of input channel in a tile
-    n_epochs = 90                                                       #####
-    input_size = 7                                                      ##### height/width dim of a tile
-    padding = 'same'                                                    #####
-    activation = 'relu'                                                 #####
-    pooling = 'avgpool'                                                 #####
-    patience = 10                                                       ##### early stopping counter patient set to 10 epoch
-    start_earlyStopping_at_epoch = 40                                   ##### early stopping will initialize after 40 epochs
-
-
-    # Default model architecture
-    default_params = {
-        'filters': [64, 64],                                          ##### convolutional layers
-        'kernel_size': [5, 5],                                        ##### kernel size for each Conv layer
-        'fc_units': [256, 32],                                        ##### fully connected layer
-        'lr': 0.0003011646825680101,                                  ##### learning rate
-        'weight_decay': 6.86933341560855e-05,                         ##### weight decay
-        'dropout': 0.5                                                ##### dropout rate
-    }
-
-
-    # Model switches
-    tune_params = False                                 ################################################################
-    n_trials_for_tuning = 200                           ################################################################
-    implement_earlyStopping = True                     #################################################################
-    plot_hyperparam_importance = True                  #################################################################
-    skip_unstandardizing_training = True               ################################################################
-    skip_unstandardizing_testing = False                ################################################################
-
-    # Running the model
+    # ------------------------------------------------------------------------------------------------------------------
+    # 1. Training model
+    # ------------------------------------------------------------------------------------------------------------------
     trained_model, model_info = main(tile_dir_train=tile_dir_train, target_csv_train=target_csv_train,
                                      tile_dir_val=tile_dir_val, target_csv_val=target_csv_val,
                                      batch_size=batch_size,
@@ -80,12 +81,17 @@ if __name__ == '__main__':
                                      hyperparam_importance_plot_path=hyperparam_importance_plot)
 
 
-    # Plotting learning curve
+    # ------------------------------------------------------------------------------------------------------------------
+    # 2. Plotting learning curve
+    # ------------------------------------------------------------------------------------------------------------------
     plot_learning_curve(train_loss=model_info['train_losses'],
                         val_loss=model_info['val_losses'],
                         plot_save_path=leaning_curve_plot)
 
-    # Model performances on standardized data
+    # ------------------------------------------------------------------------------------------------------------------
+    # 3. Model performance evaluation
+    # ------------------------------------------------------------------------------------------------------------------
+    # model performances on standardized data
     print('\n############## Model performance on standardized data ###############\n')
 
     print('Test performance:')
@@ -95,7 +101,7 @@ if __name__ == '__main__':
          data_type='test')
 
 
-    # Model performances on unstandardized (actual) data
+    # model performances on unstandardized (actual) data
     print('########## Model performance on unstandardized (actual) data ##########\n')
 
     # for train set, just using model and storing the unstandardized results
@@ -125,9 +131,9 @@ if __name__ == '__main__':
 
     print(f'Results -> RMSE: {test_rmse:.4f}, MAE: {test_mae:.4f}, NRMSE: {test_nrmse:.4f}, R²: {test_r2:.4f}\n')
 
-    # Explainable AI plots (using SHAP)
-    skip_plot_SHAP_plot = False          ################################################################################
-
+    # ------------------------------------------------------------------------------------------------------------------
+    # 4. Explainable AI plots (using SHAP)
+    # ------------------------------------------------------------------------------------------------------------------
     # current input variables' name in the model are as follows, replaces by representative names
     # ['netGWIrr', 'peff', 'ret', 'precip', 'tmax', 'ET', 'irr_crop_frac', 'irr_cropland',
     # 'maxRH', 'minRH', 'shortRad', 'vpd', 'windVel', 'sunHr', 'sw_huc12', 'gw_perc_huc12']
@@ -143,74 +149,3 @@ if __name__ == '__main__':
                      plot_save_path=f'../../Model_run/DL_model/SHAP_summary_{model_version}.jpg',
                      feature_names=feature_names, data_type='test',
                      skip_processing=skip_plot_SHAP_plot)
-
-
-    # ####################################################################################################################
-    # ####################################################################################################################
-    # # # created dataset for each state and will test model's performance on individual state
-    #
-    # skip_perState_performance_evaluation = True   #####################################################################
-    #
-    # if not skip_perState_performance_evaluation:
-    #
-    #     # # # # # # # # # # # # # # # # # # # Model performances on Kansas # # # # # # # # # # # # # # # # # # # # # # #
-    #     print('\n********************* Model performance on Kansas *********************\n')
-    #
-    #     print('Standardized performance:')
-    #
-    #     tile_dir_KS = '../../Data_main/rasters/multibands_perState/KS/train_val_test_splits/standardized/train'
-    #     target_csv_KS = '../../Data_main/rasters/multibands_perState/KS/train_val_test_splits/standardized/train/y_train.csv'
-    #
-    #     test(trained_model,
-    #          tile_dir=tile_dir_KS, target_csv=target_csv_KS,
-    #          batch_size=batch_size,
-    #          data_type='test')
-    #
-    #     print('UnStandardized performance:')
-    #     ks_rmse, ks_mae, ks_r2, ks_nrmse = \
-    #         unstandardize_save_and_test(trained_model, tile_dir=tile_dir_KS, target_csv=target_csv_KS,
-    #                                     batch_size=batch_size, data_type='test', mean_csv=mean_csv, std_csv=std_csv,
-    #                                     output_csv=f'../../Model_run/DL_model/output_csv/perState/KS_results.csv',
-    #                                     skip_processing=False)
-    #
-    #     print(f'Results -> RMSE: {ks_rmse:.4f}, MAE: {ks_mae:.4f}, NRMSE: {ks_nrmse:.4f}, R²: {ks_r2:.4f}\n')
-    #
-    #     # # # # # # # # # # # # # # # # # # Model performances on Colorado # # # # # # # # # # # # # # # # # # # # # # #
-    #     print('\n********************* Model performance on Colorado *********************\n')
-    #
-    #     print('Standardized performance:')
-    #
-    #     tile_dir_CO = '../../Data_main/rasters/multibands_perState/CO/train_val_test_splits/standardized/train'
-    #     target_csv_CO = '../../Data_main/rasters/multibands_perState/CO/train_val_test_splits/standardized/train/y_train.csv'
-    #
-    #     test(trained_model, tile_dir=tile_dir_CO, target_csv=target_csv_CO,
-    #          batch_size=batch_size, data_type='test')
-    #
-    #     print('UnStandardized performance:')
-    #     co_rmse, co_mae, co_r2, co_nrmse = \
-    #         unstandardize_save_and_test(trained_model, tile_dir=tile_dir_CO, target_csv=target_csv_CO,
-    #                                     batch_size=batch_size, data_type='test', mean_csv=mean_csv, std_csv=std_csv,
-    #                                     output_csv=f'../../Model_run/DL_model/output_csv/perState/CO_results.csv',
-    #                                     skip_processing=False)
-    #
-    #     print(f'Results -> RMSE: {co_rmse:.4f}, MAE: {co_mae:.4f}, NRMSE: {co_nrmse:.4f}, R²: {co_r2:.4f}\n')
-    #
-    #     # # # # # # # # # # # # # # # # # # Model performances on Arizona # # # # # # # # # # # # # # # # # # # # # # #
-    #     print('\n********************* Model performance on Arizona *********************\n')
-    #
-    #     print('Standardized performance:')
-    #
-    #     tile_dir_AZ = '../../Data_main/rasters/multibands_perState/AZ/train_val_test_splits/standardized/train'
-    #     target_csv_AZ = '../../Data_main/rasters/multibands_perState/AZ/train_val_test_splits/standardized/train/y_train.csv'
-    #
-    #     test(trained_model, tile_dir=tile_dir_AZ, target_csv=target_csv_AZ,
-    #          batch_size=batch_size, data_type='test')
-    #
-    #     print('UnStandardized performance:')
-    #     az_rmse, az_mae, az_r2, az_nrmse = \
-    #         unstandardize_save_and_test(trained_model, tile_dir=tile_dir_AZ, target_csv=target_csv_AZ,
-    #                                     batch_size=batch_size, data_type='test', mean_csv=mean_csv, std_csv=std_csv,
-    #                                     output_csv=f'../../Model_run/DL_model/output_csv/perState/AZ_results.csv',
-    #                                     skip_processing=False)
-    #
-    #     print(f'Results -> RMSE: {az_rmse:.4f}, MAE: {az_mae:.4f}, NRMSE: {az_nrmse:.4f}, R²: {az_r2:.4f}\n')

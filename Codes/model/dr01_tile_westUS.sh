8b415af3890e749b13a2db66228003579401dffa
mdfahimhasan <77580408+mdfahimhasan@users.noreply.github.com>
2025-02-19 09:22:10 -0700

    minor

--- Codes/model/dr01_tile_westUS.sh
+++ Codes/model/dr01_tile_westUS.sh
@@ -5,7 +5,6 @@
 #SBATCH --partition=smi_all
 #SBATCH --ntasks=30
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1 #Request 1 GPU
 #SBATCH --time=1-0
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=Fahim.Hasan@colostate.edu
